1f0d18d877f8229ca13a8e487e0ff8bf293e747c
Andy <lhr325803176@163.com>
2022-05-19 09:52:10 +0800

    add malloc lab source code

---
+++ system/memlib.c
@@ -0,0 +1,130 @@
+/*
+ * memlib.c - a module that simulates the memory system.  Needed because it 
+ *            allows us to interleave calls from the student's malloc package 
+ *            with the system's malloc package in libc.
+ */
+#include <xinu.h>
+#include "memlib.h"
+
+
+/*
+ * This constant gives the estimated performance of the libc malloc
+ * package using our traces on some reference system, typically the
+ * same kind of system the students use. Its purpose is to cap the
+ * contribution of throughput to the performance index. Once the
+ * students surpass the AVG_LIBC_THRUPUT, they get no further benefit
+ * to their score.  This deters students from building extremely fast,
+ * but extremely stupid malloc packages.
+ */
+#define AVG_LIBC_THRUPUT      600E3  /* 600 Kops/sec */
+
+ /* 
+  * This constant determines the contributions of space utilization
+  * (UTIL_WEIGHT) and throughput (1 - UTIL_WEIGHT) to the performance
+  * index.  
+  */
+#define UTIL_WEIGHT .60
+
+/* 
+ * Alignment requirement in bytes (either 4 or 8) 
+ */
+#define ALIGNMENT 8  
+
+/* 
+ * Maximum heap size in bytes 
+ */
+#define MAX_HEAP (20*(1<<20))  /* 20 MB */
+
+/*****************************************************************************
+ * Set exactly one of these USE_xxx constants to "1" to select a timing method
+ *****************************************************************************/
+#define USE_FCYC   0   /* cycle counter w/K-best scheme (x86 & Alpha only) */
+#define USE_ITIMER 0   /* interval timer (any Unix box) */
+#define USE_GETTOD 1   /* gettimeofday (any Unix box) */
+
+
+/* private variables */
+static char *mem_start_brk;  /* points to first byte of heap */
+static char *mem_brk;        /* points to last byte of heap */
+static char *mem_max_addr;   /* largest legal heap address */ 
+
+/* 
+ * mem_init - initialize the memory system model
+ */
+void mem_init(void)
+{
+    /* allocate the storage we will use to model the available VM */
+    if ((mem_start_brk = (char *)malloc(MAX_HEAP)) == NULL) {
+	fprintf(stderr, "mem_init_vm: malloc error\n");
+	exit(1);
+    }
+
+    mem_max_addr = mem_start_brk + MAX_HEAP;  /* max legal heap address */
+    mem_brk = mem_start_brk;                  /* heap is empty initially */
+}
+
+/* 
+ * mem_deinit - free the storage used by the memory system model
+ */
+void mem_deinit(void)
+{
+    free(mem_start_brk);
+}
+
+/*
+ * mem_reset_brk - reset the simulated brk pointer to make an empty heap
+ */
+void mem_reset_brk()
+{
+    mem_brk = mem_start_brk;
+}
+
+/* 
+ * mem_sbrk - simple model of the sbrk function. Extends the heap 
+ *    by incr bytes and returns the start address of the new area. In
+ *    this model, the heap cannot be shrunk.
+ */
+void *mem_sbrk(int incr) 
+{
+    char *old_brk = mem_brk;
+
+    if ( (incr < 0) || ((mem_brk + incr) > mem_max_addr)) {
+	errno = ENOMEM;
+	fprintf(stderr, "ERROR: mem_sbrk failed. Ran out of memory...\n");
+	return (void *)-1;
+    }
+    mem_brk += incr;
+    return (void *)old_brk;
+}
+
+/*
+ * mem_heap_lo - return address of the first heap byte
+ */
+void *mem_heap_lo()
+{
+    return (void *)mem_start_brk;
+}
+
+/* 
+ * mem_heap_hi - return address of last heap byte
+ */
+void *mem_heap_hi()
+{
+    return (void *)(mem_brk - 1);
+}
+
+/*
+ * mem_heapsize() - returns the heap size in bytes
+ */
+size_t mem_heapsize() 
+{
+    return (size_t)(mem_brk - mem_start_brk);
+}
+
+/*
+ * mem_pagesize() - returns the page size of the system
+ */
+size_t mem_pagesize()
+{
+    return (size_t)getpagesize();
+}
