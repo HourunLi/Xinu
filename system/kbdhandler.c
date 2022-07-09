78391b07809cf01bfb1eafb913ddb0137f29b213
Andy <lhr325803176@163.com>
2022-07-09 15:44:50 +0800

    initial

---
+++ system/kbdhandler.c
@@ -0,0 +1,13 @@
+#include <xinu.h>
+
+void kbdhandler(void) 
+{
+	int ch;
+	
+	/* complete (or modify) the code s.t. it works */
+	while ((ch = /*coding here*/) >= 0) 
+	{
+		/* fill your code here*/
+	}
+
+}
